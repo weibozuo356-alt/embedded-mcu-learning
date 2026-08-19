7f6c406b984e19b83cafdeed91d77b5c449f39d7
weibozuo356-alt <weibozuo356-alt@users.noreply.github.com>
2026-08-19 22:53:54 +0800

    add C language learning notes and exercises

---
+++ 01-c-basics/02-variables-and-arithmetic/variables_and_arithmetic.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int AAA;
+    float BBB;
+    char CCC;
+    CCC = 'G';
+    AAA = 666/3 + 123*2;
+    AAA = AAA + 1;
+    BBB = 3.14;
+    printf("%d\r\n",AAA);
+    printf("%.2f\r\n",BBB);
+    printf("%c\r\n",CCC);
+    return 0;
+
+}
